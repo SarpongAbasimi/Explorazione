32f603a59e846664541041b1668e4476a6f5ebc0
Sarpong Abasimi <sabasimi@deloitte.co.uk>
2020-08-13 22:24:32 +0100

    [SA] conf for vtut

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,12 @@ import React,{ useState } from 'react';
 import { DivAnimationPage } from './Page/DivAnimationPage/DivAnimation';
 import { TodoPage } from './Page/ToDoPage/ToDoPage';
 import { Cm } from './Components/UseSprings/UseSprings';
+import { MyComponent } from './Components/Springs/Springs'
 
 function App() {
   return (
     <div>
-      <Cm/>
+      <MyComponent/>
     </div>
   );
 }
---
+++ src/Components/Springs/Springs.tsx
@@ -0,0 +1,72 @@
+import React from 'react';
+import {useSprings, animated} from 'react-spring';
+
+
+const data = [
+  {
+    id: 1,
+    from: {
+      backgroundColor: '#E6E6FA',
+      width: '10px',
+    },
+    to : {
+      backgroundColor: '#EE82EE',
+      width: '200px'
+    },
+    config:{
+      mass: 10,
+      tension: 10
+    },
+    delay: '1000'
+  },
+  {
+    id: 2,
+    from: {
+      backgroundColor: '#DA70D6',
+      width: '20px',
+    },
+    to : {
+      backgroundColor: '#87CEEB',
+      width: '250px'
+    },
+    config: {
+      friction: 40,
+      mass: 20
+    }
+  },
+  {
+    id: 3,
+    from: {
+      backgroundColor: '#BC8F8F',
+      width: '20px',
+    },
+    to : {
+      backgroundColor: '#B8860B',
+      width: '80px'
+    },
+    config: {
+      mass: 40
+    }
+  },
+]
+export const MyComponent = () => {
+  const spring = useSprings(
+    data.length,
+    data.map(({ id, ...config})=> config )
+    )
+
+  return(
+    <>
+    { spring.map((spring, index)=> <animated.div 
+    key={data[index].id}
+    style={{
+      ...spring,
+      'height': '50px',
+      'margin': '10px'
+    }}
+    >
+
+    </animated.div>)}
+    </>
+  )
+}
