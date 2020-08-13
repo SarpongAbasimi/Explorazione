5979f940da18d6842fb1ce27876da9b0011eeb97
Sarpong Abasimi <sabasimi@deloitte.co.uk>
2020-08-13 07:43:30 +0100

    [SA] usesprings

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 import React,{ useState } from 'react';
 import { DivAnimationPage } from './Page/DivAnimationPage/DivAnimation';
 import { TodoPage } from './Page/ToDoPage/ToDoPage';
-// import { PostPage } from './Page/Post/Post';
-// import { ReactSpringPage } from './Page/ReactSpring/ReactSprint';
+import { Cm } from './Components/UseSprings/UseSprings';
 
 function App() {
   return (
     <div>
-      <TodoPage />
+      <Cm/>
     </div>
   );
 }
